3e5f8eb8e304a6fc0ec60e78ed345c803b21345e
Felipe Flores <felipe.flores_external@wartsila.com>
2023-04-02 00:06:04 +0200

    Refactor: Generalize I repetition
    The repetition here is an easy win, since we already have guards for the
    bigger numbers.

--- roman-numerals/src/main/java/com/waes/palaestra/romannumerals/RomanNumeralsConverter.java
+++ roman-numerals/src/main/java/com/waes/palaestra/romannumerals/RomanNumeralsConverter.java
@@ -8,9 +8,15 @@ public class RomanNumeralsConverter {
         if (number == 6) return "VI";
         if (number == 5) return "V";
         if (number == 4) return "IV";
-        if (number == 3) return "III";
-        if (number == 2) return "II";
-        return "I";
+
+        var result = new StringBuilder();
+
+        while (number >= 1) {
+            result.append("I");
+            number-=1;
+        }
+
+        return result.toString();
     }
 
 }
