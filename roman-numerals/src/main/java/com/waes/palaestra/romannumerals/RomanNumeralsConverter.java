60236f919b693c8dca23f364f8af19af208bfb58
Felipe Flores <felipe.flores_external@wartsila.com>
2023-04-02 00:09:03 +0200

    Refactor: Remove uneeded guard conditionals for cases higher than 5
    The simplest cases of just adding the repeated letter afterwards was
    easy enough.

--- roman-numerals/src/main/java/com/waes/palaestra/romannumerals/RomanNumeralsConverter.java
+++ roman-numerals/src/main/java/com/waes/palaestra/romannumerals/RomanNumeralsConverter.java
@@ -3,14 +3,11 @@ package com.waes.palaestra.romannumerals;
 public class RomanNumeralsConverter {
 
     public static String convert(int number) {
-        if (number == 8) return "VIII";
-        if (number == 7) return "VII";
-        if (number == 6) return "VI";
         if (number == 4) return "IV";
 
         var result = new StringBuilder();
 
-        if (number == 5) {
+        if (number >= 5) {
             result.append("V");
             number-=5;
         }
