524c9bb27d5cc2e08cc3c72b024ecb59c6b2f854
Felipe Flores <felipe.flores_external@wartsila.com>
2023-04-02 00:11:55 +0200

    Refactor: Remove duplicated tests
    Since it is already covered by the other case, I can safely remove
    without losing coverage.

--- roman-numerals/src/test/java/com/waes/palaestra/romannumerals/RomanNumeralsConverterTest.java
+++ roman-numerals/src/test/java/com/waes/palaestra/romannumerals/RomanNumeralsConverterTest.java
@@ -19,21 +19,6 @@ class RomanNumeralsConverterTest {
     }
 
     @Test
-    void given1expectsI() {
-        assertEquals("I", RomanNumeralsConverter.convert(1));
-    }
-
-    @Test
-    void given2expectsII() {
-        assertEquals("II", RomanNumeralsConverter.convert(2));
-    }
-
-    @Test
-    void given3expectsIII() {
-        assertEquals("III", RomanNumeralsConverter.convert(3));
-    }
-
-    @Test
     void given4expectsIV() {
         assertEquals("IV", RomanNumeralsConverter.convert(4));
     }
