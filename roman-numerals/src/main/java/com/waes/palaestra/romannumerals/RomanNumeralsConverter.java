fc15072942ad5eb875e96bc7657c3dc4e89a2811
Felipe Flores <felipe.flores_external@wartsila.com>
2023-04-02 00:32:42 +0200

    Refactor: Relate weights and letters for the given alphabet
    In that step it gets clearer that the process of getting the desired
    letter is similiar to every condition until now.

--- roman-numerals/src/main/java/com/waes/palaestra/romannumerals/RomanNumeralsConverter.java
+++ roman-numerals/src/main/java/com/waes/palaestra/romannumerals/RomanNumeralsConverter.java
@@ -1,32 +1,43 @@
 package com.waes.palaestra.romannumerals;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class RomanNumeralsConverter {
+
+    private static final Map<Integer, String> SYMBOLS = new LinkedHashMap<>() {{
+        put(10, "X");
+        put(9, "IX");
+        put(5, "V");
+        put(4, "IV");
+        put(1, "I");
+    }};
 
     public static String convert(int number) {
         var result = new StringBuilder();
 
         while (number >= 10) {
-            result.append("X");
+            result.append(SYMBOLS.get(10));
             number-=10;
         }
 
         if (number == 9) {
-            result.append("IX");
+            result.append(SYMBOLS.get(9));
             number-=9;
         }
 
         if (number >= 5) {
-            result.append("V");
+            result.append(SYMBOLS.get(5));
             number-=5;
         }
 
         if (number == 4) {
-            result.append("IV");
+            result.append(SYMBOLS.get(4));
             number-=4;
         }
 
         while (number >= 1) {
-            result.append("I");
+            result.append(SYMBOLS.get(1));
             number-=1;
         }
 
