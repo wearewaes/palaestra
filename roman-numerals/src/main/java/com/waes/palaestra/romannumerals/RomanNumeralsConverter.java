bcc91eaf971dc673c742d538872af5276dd37e17
Felipe Flores <felipe.flores_external@wartsila.com>
2023-04-03 11:35:49 +0200

    Introduce Symbol enum
    Might having an alphabet defined somewhere be easier to understand than
    loose strings.

--- roman-numerals/src/main/java/com/waes/palaestra/romannumerals/RomanNumeralsConverter.java
+++ roman-numerals/src/main/java/com/waes/palaestra/romannumerals/RomanNumeralsConverter.java
@@ -1,24 +1,26 @@
 package com.waes.palaestra.romannumerals;
 
+import static com.waes.palaestra.romannumerals.Symbol.*;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class RomanNumeralsConverter {
 
-    private static final Map<Integer, String> SYMBOLS = new LinkedHashMap<>() {{
-        put(1000, "M");
-        put(900, "CM");
-        put(500, "D");
-        put(400, "CD");
-        put(100, "C");
-        put(90, "XC");
-        put(50, "L");
-        put(40, "XL");
-        put(10, "X");
-        put(9, "IX");
-        put(5, "V");
-        put(4, "IV");
-        put(1, "I");
+    private static final Map<Integer, Symbol> SYMBOLS = new LinkedHashMap<>() {{
+        put(1000, M);
+        put(900, CM);
+        put(500, D);
+        put(400, CD);
+        put(100, C);
+        put(90, XC);
+        put(50, L);
+        put(40, XL);
+        put(10, X);
+        put(9, IX);
+        put(5, V);
+        put(4, IV);
+        put(1, I);
     }};
 
     public static String convert(int number) {
@@ -28,7 +30,7 @@ public class RomanNumeralsConverter {
 
         for (var entry : SYMBOLS.entrySet()) {
             var weight = entry.getKey();
-            var letter = entry.getValue();
+            var letter = entry.getValue().name();
 
             while (number >= weight) {
                 result.append(letter);
---
+++ roman-numerals/src/main/java/com/waes/palaestra/romannumerals/Symbol.java
@@ -0,0 +1,17 @@
+package com.waes.palaestra.romannumerals;
+
+public enum Symbol {
+    I,
+    IV,
+    V,
+    IX,
+    X,
+    XL,
+    L,
+    XC,
+    C,
+    CD,
+    D,
+    CM,
+    M;
+}
