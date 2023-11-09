b2d6d5faac5db9b99258bd6c2fc3c6b4ad815a96
Felipe Flores <felipe.flores_external@wartsila.com>
2023-11-09 10:24:50 +0100

    Add first traning agreements

--- fizz-buzz/src/test/java/com/waes/palaestra/fizzbuzz/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/com/waes/palaestra/fizzbuzz/FizzBuzzTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class FizzBuzzTest {
 
     @Test
-    void howShouldIStart() {
+    void givenSomethingShouldReturnWhatever() {
         assertEquals(1, 2);
     }
 }
