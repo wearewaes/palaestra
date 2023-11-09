24286d11262c15140a1156817c8d045d21884f06
Felipe Flores <felipe.flores_external@wartsila.com>
2023-11-09 12:38:37 +0100

    Solve FizzBuzz exericise

--- fizz-buzz/src/main/java/com/waes/palaestra/fizzbuzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/waes/palaestra/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,11 @@ package com.waes.palaestra.fizzbuzz;
 
 public class FizzBuzz {
 
+    public static String calculate(int number) {
+        if (number <= 0) throw new IllegalArgumentException("Number equal/smaller than 0 is not allowed");
+        if (number % 15 == 0) return "FizzBuzz";
+        if (number % 5 == 0) return "Buzz";
+        if (number % 3 == 0) return "Fizz";
+        return String.valueOf(number);
+    }
 }
