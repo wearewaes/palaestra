2fd481f140bbde315a4c5aef84af0082972d35b9
Felipe Flores <felipe.flores_external@wartsila.com>
2023-11-09 12:38:37 +0100

    Solve FizzBuzz exericise

--- fizz-buzz/src/test/java/com/waes/palaestra/fizzbuzz/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/com/waes/palaestra/fizzbuzz/FizzBuzzTest.java
@@ -1,13 +1,47 @@
 package com.waes.palaestra.fizzbuzz;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class FizzBuzzTest {
 
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 9, 21})
+    void givenMultipleOf3ShouldReturnFizz(int multipleOf3) {
+        assertEquals("Fizz", FizzBuzz.calculate(multipleOf3));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 20, 25})
+    void givenMultipleOf5ShouldReturnBuzz(int multipleOf5) {
+        assertEquals("Buzz", FizzBuzz.calculate(multipleOf5));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 4, 7})
+    void givenNonMultipleOf3AndOr5ShouldReturnTheNumber(int non3or5Multiple) {
+        assertEquals(String.valueOf(non3or5Multiple), FizzBuzz.calculate(non3or5Multiple));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {15, 30, 45})
+    void givenMultipleOf3And5ShouldReturnFizzBuzz(int multiple3And5) {
+        assertEquals("FizzBuzz", FizzBuzz.calculate(multiple3And5));
+    }
+
     @Test
-    void givenSomethingShouldReturnWhatever() {
-        assertEquals(1, 2);
+    void givenZeroShouldRaiseException() {
+        var exception = assertThrows(IllegalArgumentException.class, () -> FizzBuzz.calculate(0));
+        assertEquals("Number equal/smaller than 0 is not allowed", exception.getMessage());
+    }
+
+    @Test
+    void givenNegativeNumberShouldRaiseException() {
+        var exception = assertThrows(IllegalArgumentException.class, () -> FizzBuzz.calculate(-3));
+        assertEquals("Number equal/smaller than 0 is not allowed", exception.getMessage());
     }
 }
