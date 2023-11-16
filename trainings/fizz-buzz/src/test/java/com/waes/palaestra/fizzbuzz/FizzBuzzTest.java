7f3b2316403b237db59e5fac1adc9dd80fb79afc
Felipe Flores <felipe.flores_external@wartsila.com>
2023-11-16 23:38:19 +0100

    Split FizzBuzz into solution and training

---
+++ trainings/fizz-buzz/src/test/java/com/waes/palaestra/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package com.waes.palaestra.fizzbuzz;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+    @Test
+    void givenSomethingShouldReturnSomethingElse() {
+        assertEquals(1, 2);
+    }
+}
