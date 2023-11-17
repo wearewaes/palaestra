15a1f2b1ccfb57053bd9ceaa43ed6d0806919e6c
Felipe Flores <felipe.flores_external@wartsila.com>
2023-11-17 14:07:48 +0100

    Preparation session 3

--- trainings/roman-numerals/src/test/java/com/waes/palaestra/romannumerals/RomanNumeralsConverterTest.java
+++ trainings/roman-numerals/src/test/java/com/waes/palaestra/romannumerals/RomanNumeralsConverterTest.java
@@ -2,12 +2,108 @@ package com.waes.palaestra.romannumerals;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RomanNumeralsConverterTest {
 
-    @Test
-    void givenSoemthingShouldReturnSomethingElse() {
-        assertEquals(1, 2);
+    @ParameterizedTest
+    @CsvSource({
+        "1, I",
+        "2, II",
+        "3, III",
+    })
+    void shouldConvertCorrectlyWithOnlySymbolI(int numberToTest, String expectedRoman) {
+        assertEquals(expectedRoman, RomanNumeralsConverter.convert(numberToTest));
     }
+
+    @ParameterizedTest
+    @CsvSource({
+        "4, IV",
+        "5, V",
+        "6, VI",
+        "7, VII",
+        "8, VIII",
+    })
+    void shouldConvertCorrectlyWithSymbolV(int numberToTest, String expectedRoman) {
+        assertEquals(expectedRoman, RomanNumeralsConverter.convert(numberToTest));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "9, IX",
+        "10, X",
+        "13, XIII",
+        "14, XIV",
+        "16, XVI",
+        "17, XVII",
+        "21, XXI",
+        "25, XXV",
+        "28, XXVIII",
+        "32, XXXII",
+        "39, XXXIX",
+    })
+    void shouldConvertCorrectlyWithSymbolX(int numberToTest, String expectedRoman) {
+        assertEquals(expectedRoman, RomanNumeralsConverter.convert(numberToTest));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "43, XLIII",
+        "50, L",
+        "54, LIV",
+        "61, LXI",
+        "75, LXXV",
+        "89, LXXXIX",
+    })
+    void shouldConvertCorrectlyWithSymbolL(int numberToTest, String expectedRoman) {
+        assertEquals(expectedRoman, RomanNumeralsConverter.convert(numberToTest));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "93, XCIII",
+        "100, C",
+        "104, CIV",
+        "201, CCI",
+        "255, CCLV",
+        "399, CCCXCIX",
+    })
+    void shouldConvertCorrectlyWithSymbolC(int numberToTest, String expectedRoman) {
+        assertEquals(expectedRoman, RomanNumeralsConverter.convert(numberToTest));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "403, CDIII",
+        "500, D",
+        "604, DCIV",
+        "701, DCCI",
+        "855, DCCCLV",
+        "899, DCCCXCIX",
+    })
+    void shouldConvertCorrectlyWithSymbolD(int numberToTest, String expectedRoman) {
+        assertEquals(expectedRoman, RomanNumeralsConverter.convert(numberToTest));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "903, CMIII",
+        "1000, M",
+        "2604, MMDCIV",
+        "2701, MMDCCI",
+        "3855, MMMDCCCLV",
+        "4000, MMMM",
+    })
+    void shouldConvertCorrectlyWithSymbolM(int numberToTest, String expectedRoman) {
+        assertEquals(expectedRoman, RomanNumeralsConverter.convert(numberToTest));
+    }
+
+    //TODO Handle Zero
+    //TODO Handle Negative Numbers
+    //TODO Handle Numbers from 5000 and forward
+    //TODO Refactor Primitive Obsession
+    //TODO Make Symbols enum have unique symbols only and no combinations work around
+    //TODO Refactor code into "functional-like" style
+    //TODO Return Result[Success|Failure] instead of throwing exceptions
 }
