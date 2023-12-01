921624a4f144a72af5551fbf3d97b5463999e509
Felipe Flores <felipe.flores_external@wartsila.com>
2023-12-01 15:53:05 +0100

    Extract Symbol enum

--- trainings/roman-numerals/src/main/java/com/waes/palaestra/romannumerals/RomanNumeralsConverter.java
+++ trainings/roman-numerals/src/main/java/com/waes/palaestra/romannumerals/RomanNumeralsConverter.java
@@ -2,28 +2,6 @@ package com.waes.palaestra.romannumerals;
 
 public class RomanNumeralsConverter {
 
-    enum Symbols {
-        M(1000),
-        CM(900),
-        D(500),
-        CD(400),
-        C(100),
-        XC(90),
-        L(50),
-        XL(40),
-        X(10),
-        IX(9),
-        V(5),
-        IV(4),
-        I(1);
-
-        final int weight;
-
-        Symbols(int weight) {
-            this.weight = weight;
-        }
-    }
-
     public RomanNumeral convert(int cardinalNumber) {
         if (cardinalNumber <= 0) {
             throw new NumberTooLow();
@@ -33,7 +11,7 @@ public class RomanNumeralsConverter {
         }
 
         var result = new StringBuilder();
-        for (var symbol:Symbols.values()) {
+        for (var symbol: RomanSymbol.values()) {
             while (cardinalNumber >= symbol.weight) {
                 result.append(symbol.name());
                 cardinalNumber -= symbol.weight;
